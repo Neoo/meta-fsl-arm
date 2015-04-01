Neoo/meta-fsl-arm

commit f69b21d39feca9a4d71d53980fc5c9917ba6c5ac

fsl-kernel-localversion.bbclass: Fix building using out-of-tree
Since OE-Core:df9975f4 (kernel: Fix non linux-yocto builds) the way
the build of kernel is done has change. Now the source code is moved
placed at STAGING_KERNEL_DIR and the build is done at ${WORKDIR}/build
which is B.

This patch adapts the configure task prepending to cope with those changes.

Change-Id: I9ea4254ffd56e4b7201daf9a90b52756a399c0f6
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

classes/fsl-kernel-localversion.bbclass
@@ -16,24 +16,24 @@ kernel_conf_variable() {
 	CONF_SED_SCRIPT="$CONF_SED_SCRIPT /CONFIG_$1[ =]/d;"
 	if test "$2" = "n"
 	then
-		echo "# CONFIG_$1 is not set" >> ${S}/.config
+		echo "# CONFIG_$1 is not set" >> ${B}/.config
 	else
-		echo "CONFIG_$1=$2" >> ${S}/.config
+		echo "CONFIG_$1=$2" >> ${B}/.config
 	fi
 }
 
 do_configure_prepend() {
-	echo "" > ${S}/.config
+	echo "" > ${B}/.config
 	CONF_SED_SCRIPT=""
 
 	kernel_conf_variable LOCALVERSION "\"${LOCALVERSION}\""
 	kernel_conf_variable LOCALVERSION_AUTO y
 
-	sed -e "${CONF_SED_SCRIPT}" < '${WORKDIR}/defconfig' >> '${S}/.config'
+	sed -e "${CONF_SED_SCRIPT}" < '${WORKDIR}/defconfig' >> '${B}/.config'
 
 	if [ "${SCMVERSION}" = "y" ]; then
 		# Add GIT revision to the local version
-		head=`git rev-parse --verify --short HEAD 2> /dev/null`
-		printf "%s%s" +g $head > ${S}/.scmversion
+		head=`git --git-dir=${S}/.git rev-parse --verify --short HEAD 2> /dev/null`
+		printf "%s%s" +g $head > ${B}/.scmversion
 	fi
 }

commit 3cf3bcb95d63fbcf5ab9ea943afc0f4d8ec1921a

fsl-kernel-localversion.bbclass: Fix Git hash in version
The .scmversion file needs to be located in the Git source, otherwise
the build system does not use it.

This fixes the regression introduced in f69b21d
(fsl-kernel-localversion.bbclass: Fix building using out-of-tree).

Change-Id: I7f06279f5b1f9aee18d1a5a39a22b27f4c34f365
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

classes/fsl-kernel-localversion.bbclass
@@ -7,7 +7,7 @@
 #  SCMVERSION        Puts the Git hash in kernel local version
 #  LOCALVERSION      Value used in LOCALVERSION (default to '+fslc')
 #
-# Copyright 2014 (C) O.S. Systems Software LTDA.
+# Copyright 2014, 2015 (C) O.S. Systems Software LTDA.
 
 SCMVERSION ??= "y"
 LOCALVERSION ??= "+fslc"
@@ -34,6 +34,6 @@ do_configure_prepend() {
 	if [ "${SCMVERSION}" = "y" ]; then
 		# Add GIT revision to the local version
 		head=`git --git-dir=${S}/.git rev-parse --verify --short HEAD 2> /dev/null`
-		printf "%s%s" +g $head > ${B}/.scmversion
+		printf "%s%s" +g $head > ${S}/.scmversion
 	fi
 }